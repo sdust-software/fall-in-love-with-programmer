d9610c9c9d22585007aadefce24f975999251686
yanming <272574797@qq.com>
2016-08-06 16:25:28 +0800

    First commit.

---
+++ app/src/androidTest/java/com/sudst/administrator/fall_in_love_with_programmer/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.sudst.administrator.fall_in_love_with_programmer;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/sudst/administrator/fall_in_love_with_programmer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sudst.administrator.fall_in_love_with_programmer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
